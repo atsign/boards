9c4d28f75626be8705467117e8a8b37bfe8b5147
Andrew Iafrate <a.a.iafrate@gmail.com>
2016-03-06 14:24:48 -0500

    Add grunt, sass, bourbon, neat, and set up stylesheet directory structure

---
+++ src/Boards/gruntfile.js
@@ -0,0 +1,29 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'wwwroot/css/styles.css': 'wwwroot/css/sass/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: ['wwwroot/css/sass/**/*.scss'],
+            tasks: ['sass']
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
