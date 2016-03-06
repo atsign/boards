201d8d788fcc35513488757305e7ddec603b9fdf
Andrew Iafrate <a.a.iafrate@gmail.com>
2016-03-06 13:27:26 -0500

    Initial commit

---
+++ src/Boards/Controllers/Web/AppController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boards.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
