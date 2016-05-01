873a46164c7d381d58806029b9156f92d973a71a
Andrew Iafrate <a.a.iafrate@gmail.com>
2016-05-01 17:21:43 -0400

    Add phase model and migration

---
+++ src/Boards/Models/Phase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boards.Models
+{
+    public class Phase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public int BoardId { get; set; }
+    }
+}
